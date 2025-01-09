088d27b00d440fc4e52dcf1a499954cf89546e07
yanakorsak <kazantip2310@gmail.com>
2025-01-09 13:24:42 +0100

    bucles while

---
+++ script.js
@@ -0,0 +1,22 @@
+let sum = 1000
+let persent = 15
+const result=2000;
+let count =0
+while (sum< result) {
+  sum = sum+sum*(persent/100)
+  count =count +1
+  
+}
+console.log(sum,count);
+
+
+// let sum = 1000
+// let persent = 15
+// const result=2000;
+// let count =0
+while (count< 10) {
+  sum = sum+sum*(persent/100)
+  count =count +1
+  
+}
+console.log(sum,count);
